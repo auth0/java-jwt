c7da0c735e7cd04ebbf2eb632acd459c08a54800
gfernandez <gfernandez@qdqmedia.com>
2016-09-05 09:23:46 +0200

    add param header

--- src/main/java/com/auth0/jwt/JWTSigner.java
+++ src/main/java/com/auth0/jwt/JWTSigner.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
-//import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
