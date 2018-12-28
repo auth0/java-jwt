7cfb8c5a73808c470d31e1cdc83a14c05f1580f5
Martin O'Connor <martin@entourage.ie>
2018-12-28 10:07:18 -0500

    Verify a DecodedJWT. Allows verification of a DecodedJWT previously obtained by invoking JWT.decode(). This alleviates the user from having to recompute the values of the DecodedJWT in order to validate the token. Reopens #279

--- lib/src/main/java/com/auth0/jwt/interfaces/JWTVerifier.java
+++ lib/src/main/java/com/auth0/jwt/interfaces/JWTVerifier.java
@@ -13,4 +13,13 @@ public interface JWTVerifier {
    * @throws JWTVerificationException if any of the verification steps fail
    */
   DecodedJWT verify(String token) throws JWTVerificationException;
+
+  /**
+   * Performs the verification against the given decoded JWT
+   *
+   * @param jwt to verify.
+   * @return a verified and decoded JWT.
+   * @throws JWTVerificationException if any of the verification steps fail
+   */
+  DecodedJWT verify(DecodedJWT jwt) throws JWTVerificationException;
 }
