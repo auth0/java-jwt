05b54499be0cc06478467d66c801d9bc3da3fecc
Poovamraj T T <poovamraj@gmail.com>
2022-05-03 15:46:47 +0200

    Added protection against CVE-2022-21449

--- lib/src/main/java/com/auth0/jwt/algorithms/ECDSAAlgorithm.java
+++ lib/src/main/java/com/auth0/jwt/algorithms/ECDSAAlgorithm.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.ECDSAKeyProvider;
 
+import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
@@ -46,6 +47,7 @@ class ECDSAAlgorithm extends Algorithm {
             if (publicKey == null) {
                 throw new IllegalStateException("The given Public Key is null.");
             }
+            validateSignatureStructure(signatureBytes, publicKey);
             boolean valid = crypto.verifySignatureFor(getDescription(), publicKey, jwt.getHeader(), jwt.getPayload(), JOSEToDER(signatureBytes));
 
             if (!valid) {
@@ -140,13 +142,42 @@ class ECDSAAlgorithm extends Algorithm {
         return joseSignature;
     }
 
-    //Visible for testing
-    byte[] JOSEToDER(byte[] joseSignature) throws SignatureException {
+    /**
+     * Added check for extra protection against CVE-2022-21449.
+     * This method ensures the signature's structure is as expected.
+     *
+     * @param joseSignature is the signature from the JWT
+     * @param publicKey public key used to verify the JWT
+     * @throws SignatureException if the signature's structure is not as per expectation
+     */
+    // Visible for testing
+    void validateSignatureStructure(byte[] joseSignature, ECPublicKey publicKey) throws SignatureException {
+        // check signature length, moved this check from JOSEToDER method
         if (joseSignature.length != ecNumberSize * 2) {
             throw new SignatureException("Invalid JOSE signature format.");
         }
 
-        // Retrieve R and S number's length and padding.
+        if (isAllZeros(joseSignature)) {
+            throw new SignatureException("Invalid Signature: All Zeros.");
+        }
+
+        // get R
+        byte[] rBytes = new byte[ecNumberSize];
+        System.arraycopy(joseSignature, 0, rBytes, 0, ecNumberSize);
+        BigInteger r = new BigInteger(1, rBytes);
+        if(isAllZeros(rBytes)) {
+            throw new SignatureException("Invalid Signature: All Zeros for R value.");
+        }
+
+        // get S
+        byte[] sBytes = new byte[ecNumberSize];
+        System.arraycopy(joseSignature, ecNumberSize, sBytes, 0, ecNumberSize);
+        BigInteger s = new BigInteger(1, sBytes);
+        if(isAllZeros(sBytes)) {
+            throw new SignatureException("Invalid Signature: All Zeros for S value.");
+        }
+
+        //moved this check from JOSEToDER method
         int rPadding = countPadding(joseSignature, 0, ecNumberSize);
         int sPadding = countPadding(joseSignature, ecNumberSize, joseSignature.length);
         int rLength = ecNumberSize - rPadding;
@@ -156,6 +187,29 @@ class ECDSAAlgorithm extends Algorithm {
         if (length > 255) {
             throw new SignatureException("Invalid JOSE signature format.");
         }
+
+        // check for 0 r or s here since we have the values
+        if (BigInteger.ZERO.equals(r) || BigInteger.ZERO.equals(s)) {
+            throw new SignatureException("R or S value cannot be zero.");
+        }
+
+        BigInteger order = publicKey.getParams().getOrder();
+
+        // R and S must be less than N
+        if (order.compareTo(r) < 1 || order.compareTo(s) < 1) {
+            throw new SignatureException("The difference between R or S value and order should be greater than one.");
+        }
+    }
+
+    //Visible for testing
+    byte[] JOSEToDER(byte[] joseSignature) throws SignatureException {
+        // Retrieve R and S number's length and padding.
+        int rPadding = countPadding(joseSignature, 0, ecNumberSize);
+        int sPadding = countPadding(joseSignature, ecNumberSize, joseSignature.length);
+        int rLength = ecNumberSize - rPadding;
+        int sLength = ecNumberSize - sPadding;
+
+        int length = 2 + rLength + 2 + sLength;
 
         final byte[] derSignature;
         int offset;
@@ -203,6 +257,15 @@ class ECDSAAlgorithm extends Algorithm {
         }
 
         return derSignature;
+    }
+
+    private boolean isAllZeros(byte[] bytes) {
+        for (byte b : bytes) {
+            if (b != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private int countPadding(byte[] bytes, int fromIndex, int toIndex) {
