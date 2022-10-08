20dbf7cdb4a35b9d95562c0e135e52669a549b59
Noel Trout <noel.trout@avinode.com>
2022-10-08 09:49:29 +0200

    Add benchmark for decoder and cleanup build file

---
+++ lib/src/jmh/java/com/auth0/jwt/benchmark/JWTDecoderBenchmark.java
@@ -0,0 +1,18 @@
+package com.auth0.jwt.benchmark;
+
+import com.auth0.jwt.JWT;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.infra.Blackhole;
+
+public class JWTDecoderBenchmark {
+
+    private static final String TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    public void throughputDecodeTime(Blackhole blackhole) {
+        blackhole.consume(JWT.decode(TOKEN));
+    }
+}
