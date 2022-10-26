e5274494145afe9b77648edc06ca55be97ccfbe6
li.he <li.he@thoughtworks.com>
2022-10-26 17:39:56 +0800

    feat: add base64 decode test

--- lib/src/test/java/com/auth0/jwt/JWTDecoderTest.java
+++ lib/src/test/java/com/auth0/jwt/JWTDecoderTest.java
@@ -30,6 +30,12 @@ public class JWTDecoderTest {
         DecodedJWT jwt = JWT.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ");
         assertThat(jwt.getSubject(), is(notNullValue()));
         assertThat(jwt.getSubject(), is("1234567890"));
+
+        jwt = JWT.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
+            ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9" +
+            ".TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ", false);
+        assertThat(jwt.getSubject(), is(notNullValue()));
+        assertThat(jwt.getSubject(), is("1234567890"));
     }
 
     // Exceptions
